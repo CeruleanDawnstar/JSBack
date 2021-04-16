18708cfece4686815d7d45f39ee77be9740af7b0
atacanjacques <atacan.jacques@gmail.com>
2021-04-16 14:35:49 +0200

    Full CRUD

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,13 +1,17 @@
+const {
+    findByIdAndUpdate
+} = require('../models/postModel');
 const Post = require('../models/postModel');
 
 exports.listAllPosts = (req, res) => {
     Post.find({}, (error, posts) => {
-        if(error){
+        if (error) {
             res.status(500);
             console.log(error);
-            res.json({message: "Erreur serveur."});
-        }
-        else {
+            res.json({
+                message: "Erreur serveur."
+            });
+        } else {
             res.status(200);
             res.json(posts);
         }
@@ -27,6 +31,55 @@ exports.createAPost = (req, res) => {
         } else {
             res.status(201);
             res.json(post);
+        }
+    });
+}
+
+exports.getAPost = (req, res) => {
+    Post.findById(req.params.id_post, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            });
+        } else {
+            res.status(200);
+            res.json(post);
+        }
+    })
+}
+
+exports.updateAPost = (req, res) => {
+    Post.findByIdAndUpdate(req.params.id_post, req.body, {
+        new: true
+    }, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            });
+        } else {
+            res.status(200);
+            res.json(post);
+        }
+    })
+}
+
+exports.deleteAPost = (req, res) => {
+    Post.findByIdAndDelete(req.params.id_post, (error) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            });
+        } else {
+            res.status(200);
+            res.json({
+                message: "Article supprimé"
+            });
         }
     });
 }
