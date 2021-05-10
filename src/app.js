f8fe0d3bcf9fa57544305d209d9e631cd1fb64f3
atacanjacques <atacan.jacques@gmail.com>
2021-05-10 16:32:32 +0200

    Add cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ const express = require('express');
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const cors = require('cors');
+
 const server = express();
+
+server.use(cors({
+    credentials: true,
+    origin: 'http://localhost'
+}));
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://mongo/apinode');
